e80a2894d27c51098f7d306cc844fc233a2be68c
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-26 16:01:23 +0300

    hash password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Function that hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Function that checks is a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
