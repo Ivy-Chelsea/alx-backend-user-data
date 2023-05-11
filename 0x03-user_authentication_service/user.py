40ee126d104d63c333c3f4cfe33f902f21359b5e
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-05-11 19:57:58 +0300

    users

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env pytho3
+"""
+User model module
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Class representing a record from user table
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Colum(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Colum(String(250), nullable(True)
+    reset_token = Column(String(250), nullable=True)
